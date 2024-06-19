3c614500a44580ef22d986d37e546518d512480e
thangtran113 <hotran1801@gmail.com>
2024-06-19 18:10:16 +0700

    init project

--- app/src/main/java/com/android19/videoplayer/MainActivity.kt
+++ app/src/main/java/com/android19/videoplayer/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.android19.videoplayer
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.android19.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        requestRuntimePermission()
+        setFragment(VideosFragment())
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.videoView -> setFragment(VideosFragment())
+                R.id.folderView -> setFragment(FoldersFragment())
+            }
+            return@setOnItemSelectedListener true
         }
+
+    }
+    private fun setFragment(fragment:Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentFL,fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
+    }
+    private fun requestRuntimePermission():Boolean{
+        if (ActivityCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), 13)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 13)
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Grandted", Toast.LENGTH_SHORT).show()
+            else
+                ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),13)
     }
 }
