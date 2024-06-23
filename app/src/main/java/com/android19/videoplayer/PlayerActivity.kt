2c9afd39828576356de7c1ecad921c1e7af4ef8d
phucdaozz123 <2151163713@e.tlu.edu.vn>
2024-06-24 00:14:57 +0700

    Update buttons
    Fullscreen mode on & off, show & hide controller,  lock & unlock controller

--- app/src/main/java/com/android19/videoplayer/PlayerActivity.kt
+++ app/src/main/java/com/android19/videoplayer/PlayerActivity.kt
@@ -1,7 +1,10 @@
 package com.android19.videoplayer
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -12,20 +15,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.C
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.SimpleExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import com.android19.videoplayer.databinding.ActivityPlayerBinding
 
 @UnstableApi
 class PlayerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlayerBinding
-
+    private lateinit var runnable: Runnable
     companion object {
         lateinit var player: SimpleExoPlayer
         lateinit var playerList: ArrayList<Video>
         var position: Int = -1
         var isPlayerInitialized = false
+        private var repeat : Boolean = false
+        private var isFullScreen: Boolean = false
+        private var isLocked: Boolean = false
+
     }
 
     @OptIn(UnstableApi::class)
@@ -58,6 +68,7 @@ class PlayerActivity : AppCompatActivity() {
         initializeBinding()
     }
 
+    @SuppressLint("PrivateResource")
     private fun initializeLayout() {
         when (intent.getStringExtra("class")) {
             "AllVideos" -> {
@@ -69,9 +80,15 @@ class PlayerActivity : AppCompatActivity() {
             }
         }
         createPlayer()
+        if(repeat){
+            binding.repeatButton.setImageResource(androidx.media3.ui.R.drawable.exo_legacy_controls_repeat_all)
+        }
+        else binding.repeatButton.setImageResource(androidx.media3.ui.R.drawable.exo_legacy_controls_repeat_off)
     }
 
+    @SuppressLint("PrivateResource")
     private fun initializeBinding() {
+
         binding.backButton.setOnClickListener {
             finish()
         }
@@ -88,10 +105,53 @@ class PlayerActivity : AppCompatActivity() {
         binding.previousButton.setOnClickListener{
             nextPreviousVideo(isNext = false)
         }
+        binding.repeatButton.setOnClickListener {
+            if (repeat) {
+                repeat = false
+                player.repeatMode = Player.REPEAT_MODE_OFF
+                binding.repeatButton.setImageResource(androidx.media3.ui.R.drawable.exo_legacy_controls_repeat_off)
+            } else {
+                repeat = false
+                player.repeatMode = Player.REPEAT_MODE_ONE
+                binding.repeatButton.setImageResource(androidx.media3.ui.R.drawable.exo_legacy_controls_repeat_all)
+            }
+        }
+        binding.fullscreenButton.setOnClickListener{
+            if(isFullScreen){
+                isFullScreen = false
+                playInFullScreen(enable = false)
+            }else{
+                isFullScreen = true
+                playInFullScreen(enable = true)
+            }
+        }
+        binding.lockButton.setOnClickListener{
+            if(!isLocked){
+                //ẩn
+                isLocked = true
+                binding.playerView.hideController()
+                binding.playerView.useController = false
+                binding.lockButton.setImageResource(R.drawable.locked)
+        }
+            else{
+                //hien
+                isLocked = false
+                binding.playerView.useController = true
+                binding.playerView.showController()
+                binding.lockButton.setImageResource(R.drawable.open_lock_icon)
+            }
+        }
+
     }
 
     @OptIn(UnstableApi::class)
     private fun createPlayer() {
+        try {
+            player.release()
+        }
+        catch(e:Exception){}
+
+
         binding.videoTilte.text = playerList[position].title
         binding.videoTilte.isSelected = true
         player = SimpleExoPlayer.Builder(this).build()
@@ -101,6 +161,14 @@ class PlayerActivity : AppCompatActivity() {
         player.prepare()
         player.play()
         isPlayerInitialized = true
+        player.addListener(object : Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                if(playbackState == Player.STATE_ENDED) nextPreviousVideo()
+            }
+        })
+        playInFullScreen(enable = isFullScreen)
+        setVisibility()
     }
 
     override fun onStart() {
@@ -135,17 +203,47 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun setPosition(isIncrement: Boolean = true) {
-        if (isIncrement){
-            if(playerList.size -1 == position)
-                position = 0
-            else ++position
+        if(!repeat){
+            if (isIncrement){
+                if(playerList.size -1 == position)
+                    position = 0
+                else ++position
+            }else{
+                if(position == 0)
+                    position = playerList.size - 1
+                else --position
+            }
+        }
+    }
+    @SuppressLint("PrivateResource")
+    private fun playInFullScreen(enable:Boolean){
+        if(enable){
+            binding.playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+            player.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+            binding.fullscreenButton.setImageResource(androidx.media3.ui.R.drawable.exo_ic_fullscreen_exit)
         }else{
-            if(position == 0)
-                position = playerList.size - 1
-            else --position
+            binding.playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+            player.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT
+            binding.fullscreenButton.setImageResource(R.drawable.fullscreen_icon)
         }
     }
 
+    private fun setVisibility(){
+        runnable = Runnable{
+            if(binding.playerView.isControllerFullyVisible) changeVisibility(View.VISIBLE)
+            else changeVisibility(View.INVISIBLE)
+            Handler(Looper.getMainLooper()).postDelayed(runnable,300)
+        }
+        Handler(Looper.getMainLooper()).postDelayed(runnable,0)
+}
+    private fun changeVisibility(visibility:Int){
+        binding.topController.visibility = visibility
+        binding.bottomController.visibility = visibility
+        binding.playButton.visibility = visibility
+
+        if(isLocked) binding.lockButton.visibility = View.VISIBLE
+        else    binding.lockButton.visibility = visibility
+    }
     override fun onDestroy() {
         super.onDestroy()
         if (isPlayerInitialized) {
