709f87838fa174060ed414a5592b747b83c91547
phucdaozz123 <2151163713@e.tlu.edu.vn>
2024-06-23 22:36:07 +0700

    Add player button UI & functions
    Play, pause, next/previous video, auto fullscreen mode when opened

--- app/src/main/java/com/android19/videoplayer/PlayerActivity.kt
+++ app/src/main/java/com/android19/videoplayer/PlayerActivity.kt
@@ -1,12 +1,17 @@
 package com.android19.videoplayer
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.SimpleExoPlayer
@@ -20,14 +25,29 @@ class PlayerActivity : AppCompatActivity() {
         lateinit var player: SimpleExoPlayer
         lateinit var playerList: ArrayList<Video>
         var position: Int = -1
+        var isPlayerInitialized = false
     }
 
     @OptIn(UnstableApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPlayerBinding.inflate(layoutInflater) // Initialize the binding
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode=WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+
+
         this.enableEdgeToEdge()
+        //for immersive mode
+        setTheme(R.style.playerActivityTheme)
         setContentView(binding.root)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.root).let {
+                controller -> controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v: View, insets: WindowInsetsCompat ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -35,18 +55,39 @@ class PlayerActivity : AppCompatActivity() {
         }
 
         initializeLayout()
+        initializeBinding()
     }
 
     private fun initializeLayout() {
-        when(intent.getStringExtra("class")) {
+        when (intent.getStringExtra("class")) {
             "AllVideos" -> {
                 playerList = ArrayList(MainActivity.videoList)
             }
+
             "FolderActivity" -> {
                 playerList = ArrayList(FoldersActivity.currentFolderVideo)
             }
         }
         createPlayer()
+    }
+
+    private fun initializeBinding() {
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+        binding.playButton.setOnClickListener {
+            if (player.isPlaying) {
+                pauseVideo()
+            } else {
+                playVideo()
+            }
+        }
+        binding.nextButton.setOnClickListener{
+            nextPreviousVideo()
+        }
+        binding.previousButton.setOnClickListener{
+            nextPreviousVideo(isNext = false)
+        }
     }
 
     @OptIn(UnstableApi::class)
@@ -59,22 +100,57 @@ class PlayerActivity : AppCompatActivity() {
         player.setMediaItem(mediaItem)
         player.prepare()
         player.play()
+        isPlayerInitialized = true
     }
 
     override fun onStart() {
         super.onStart()
-        if (player == null) {
+        if (!isPlayerInitialized) {
             createPlayer()
         }
     }
 
     override fun onStop() {
         super.onStop()
-        player.release()
+        if (isPlayerInitialized) {
+            player.release()
+            isPlayerInitialized = false
+        }
+    }
+
+    private fun playVideo() {
+        binding.playButton.setImageResource(R.drawable.pause_icon)
+        player.play()
+    }
+
+    private fun pauseVideo() {
+        binding.playButton.setImageResource(R.drawable.play_icon)
+        player.pause()
+    }
+
+    private fun nextPreviousVideo(isNext: Boolean = true) {
+        if (isNext) setPosition()
+        else setPosition(isIncrement = false)
+        createPlayer()
+    }
+
+    private fun setPosition(isIncrement: Boolean = true) {
+        if (isIncrement){
+            if(playerList.size -1 == position)
+                position = 0
+            else ++position
+        }else{
+            if(position == 0)
+                position = playerList.size - 1
+            else --position
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        player.release()
+        if (isPlayerInitialized) {
+            player.release()
+            isPlayerInitialized = false
+        }
     }
 }
